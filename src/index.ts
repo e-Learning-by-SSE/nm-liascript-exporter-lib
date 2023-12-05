43a659b3816f7567afaa9b63abf2572fd5ee3ca5
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2023-12-05 07:23:14 +0100

    Converted to COMMONJS module

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import liascriptify from "../node_modules/@liascript/markdownify/dist/lib";
+import liascriptify from "@liascript/markdownify/dist/lib";
 
 export default function libLiaScr(json: any) {
   return liascriptify(json) as Promise<string>;
