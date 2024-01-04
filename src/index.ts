effaae66e048f1f3eab03217ceef2440894f8b49
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-01-04 12:39:13 +0100

    Fixed import of Version 0.1.3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import liascriptify from "@liascript/markdownify/dist/lib";
+import liascriptify from "@liascript/markdownify/dist/lib.js";
 
 export default function libLiaScr(json: any) {
   return liascriptify(json) as Promise<string>;
