53cbe67d70ff6e922a0645f528d88f9e9e8c3c15
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-01-04 13:07:35 +0100

    Version 0.1.9: Hard reset to initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-import liascriptify from "@liascript/markdownify/dist/lib.js";
-
-export default function liaScriptify (json: any) {
-  return liascriptify(json) as Promise<string>;
-}
+// the default is needed as the function is the default export and needs to be named differently to be reexported in the index.ts
+export { default as libLiaScrImp } from "./LibLiaScr";
