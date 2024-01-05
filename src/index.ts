c426a0425903b417611988fd78e1891efebe918e
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2024-01-05 10:35:16 +0100

    Version 0.1.11: Based on 0.1.3

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+import liascriptify from "@liascript/markdownify/dist/lib.js";
 // the default is needed as the function is the default export and needs to be named differently to be reexported in the index.ts
-export { default as libLiaScrImp } from "./LibLiaScr";
+
+export default function liaScriptify(json: any) {
+  return liascriptify(json) as Promise<string>;
+}
